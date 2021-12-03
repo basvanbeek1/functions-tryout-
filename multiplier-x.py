92f8480d8b3f5f52e1499e85ca1b7a903aca6c47
basvanbeek1 <99069932@mydavinci.nl>
2021-12-03 10:59:55 +0100

    tafel x foutje

---
+++ multiplier-x.py
@@ -0,0 +1,13 @@
+TafelVanX = 1 
+WelkGetal = input("welk getal wil je de tafel van zien\n")
+def TafelGetal(InvoerGetal, TafelVanX):
+    while TafelVanX < 10:
+        tafelvan = InvoerGetal * TafelVanX
+        TafelVanX + 1 
+    return tafelvan
+
+    
+        
+    
+tafelvan = (WelkGetal)
+print(TafelGetal)
